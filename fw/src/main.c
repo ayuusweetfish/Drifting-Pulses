5a307508c62ee997390ccc5fb67341af58f1a131
Ayu <claire@ayu.land>
2025-08-11 14:38:58 +0800

    fw: Re-organize debugging helpers

--- fw/src/main.c
+++ fw/src/main.c
@@ -1,50 +1,12 @@
 #include "py32f0xx_hal.h"
 #include "py32f0xx_ll_rcc.h"
 #include <assert.h>
-#include <inttypes.h>
-#include <stdarg.h>
 #include <stdbool.h>
 #include <stdint.h>
-#include <stdio.h>
 
 // #define RELEASE
 
-#ifndef RELEASE
-static uint8_t debug_buf[32];
-static size_t debug_buf_ptr = 0;
-__attribute__ ((noinline, used))
-void debug_trap_line()
-{
-  *(volatile char *)debug_buf;
-}
-static inline void debug_putchar(uint8_t c)
-{
-  if (c == '\n') {
-    debug_buf[debug_buf_ptr >= sizeof debug_buf ?
-      (sizeof debug_buf - 1) : debug_buf_ptr] = '\0';
-    debug_trap_line();
-    debug_buf_ptr = 0;
-  } else if (debug_buf_ptr <= sizeof debug_buf - 1) {
-    debug_buf[debug_buf_ptr++] = c;
-  }
-}
-__attribute__ ((format(printf, 1, 2)))
-static void debug_printf(const char *restrict fmt, ...)
-{
-  static char s[32];
-  va_list args;
-  va_start(args, fmt);
-  int r = vsnprintf(s, sizeof s, fmt, args);
-  for (int i = 0; i < r && i < sizeof s - 1; i++) debug_putchar(s[i]);
-  if (r >= sizeof s) {
-    for (int i = 0; i < 3; i++) debug_putchar('.');
-    debug_putchar('\n');
-  }
-}
-#define printf(...) debug_printf(__VA_ARGS__)
-#else
-#define printf(...)
-#endif
+#include "debug_printf.h"
 
 static void spin_delay(uint32_t cycles)
 {
@@ -65,7 +27,7 @@ static void spin_delay(uint32_t cycles)
 // __attribute__ ((section(".RamFunc")))
 static inline void delay_us(uint32_t us)
 {
-  spin_delay(us * 16);
+  spin_delay(us * 8);
 }
 
 #pragma GCC push_options
@@ -100,7 +62,7 @@ int main()
 
   printf("sysclk = %lu Hz\n", HAL_RCC_GetSysClockFreq());
 
-  while (1) { printf("!!\n"); HAL_Delay(1000); }
+  while (1) { printf("!!\n"); delay_us(1000000); }
 
   // ============ ACT LED ============ //
 {
